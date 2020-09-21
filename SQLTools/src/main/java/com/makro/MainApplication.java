89f23605d5c06a10fbdc1b85eb19f4041c2863f5
ThierryMac <739196417@qq.com>
2020-09-21 16:58:38 +0800

    测试协作者

--- SQLTools/src/main/java/com/makro/MainApplication.java
+++ SQLTools/src/main/java/com/makro/MainApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 
 import com.makro.config.BootApplication;
 /**
- * 启动类
+ * SpringBoot启动类
  * @author Thierry.Zhuang
- *
+ * 
  */
 public class MainApplication {
 	public static void main(String[] args) {
