7464fd415098d2587d360edb6de9f276dae452f6
ThierryMac <739196417@qq.com>
2020-09-21 16:53:21 +0800

    测试协作用户ThierryMac

--- SQLTools/src/main/java/com/makro/MainApplication.java
+++ SQLTools/src/main/java/com/makro/MainApplication.java
@@ -3,7 +3,11 @@ package com.makro;
 import org.springframework.boot.SpringApplication;
 
 import com.makro.config.BootApplication;
-
+/**
+ * 启动类
+ * @author Thierry.Zhuang
+ *
+ */
 public class MainApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
